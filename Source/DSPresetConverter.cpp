76fdf07f228b040071762bfe1ad5b7c644697f67
David Hilowitz <dhilowitz@gmail.com>
2022-03-24 10:43:30 -0400

    SFZ2DS and EXS2DS now use the same DSPresetConverter class

--- Source/DSPresetConverter.cpp
+++ Source/DSPresetConverter.cpp
@@ -1,8 +1,8 @@
 /*
   ==============================================================================
 
-    DSPresetMaker.cpp
     Created: 18 Oct 2020 10:30:33pm
+    DSPresetConverter.cpp
     Author:  David Hilowitz
 
   ==============================================================================
@@ -10,19 +10,19 @@
 
 #include "DSPresetConverter.h"
 
-void DSPresetConverter::parseSFZValueTree(ValueTree sfz) {
-    valueTree = ValueTree("DecentSampler");
+void DSPresetConverter::parseSFZValueTree(juce::ValueTree sfz) {
+    valueTree = juce::ValueTree("DecentSampler");
     valueTree.setProperty("pluginVersion", 1, nullptr);
-    ValueTree groupsVT = ValueTree("groups");
+    juce::ValueTree groupsVT = juce::ValueTree("groups");
     translateSFZRegionProperties(sfz, groupsVT, headerLevelGlobal);
     valueTree.appendChild(groupsVT, nullptr);
     
     for (auto sfzSection : sfz) {
         if(sfzSection.hasType("group")) {
-            ValueTree dsGroup ("group");
+            juce::ValueTree dsGroup ("group");
             translateSFZRegionProperties(sfzSection, dsGroup, headerLevelGroup);
             for (auto sfzRegion : sfzSection) {
-                ValueTree dsSample("sample");
+                juce::ValueTree dsSample("sample");
                 translateSFZRegionProperties(sfzRegion, dsSample, headerLevelRegion);
                 dsGroup.appendChild(dsSample, nullptr);
             }
@@ -31,10 +31,10 @@ void DSPresetConverter::parseSFZValueTree(ValueTree sfz) {
     }
 }
 
-void DSPresetConverter::translateSFZRegionProperties(ValueTree sfzRegion, ValueTree &dsEntity, HeaderLevel level) {
+void DSPresetConverter::translateSFZRegionProperties(juce::ValueTree sfzRegion, juce::ValueTree &dsEntity, HeaderLevel level) {
     for (int i = 0; i < sfzRegion.getNumProperties(); i++) {
-        String key = sfzRegion.getPropertyName(i).toString();
-        var value =      sfzRegion.getProperty(key);
+        juce::String key = sfzRegion.getPropertyName(i).toString();
+        juce::var value =      sfzRegion.getProperty(key);
         
         if(level == headerLevelGroup && key == "group_label") {
             dsEntity.setProperty("name", value, nullptr);
@@ -106,3 +106,119 @@ void DSPresetConverter::translateSFZRegionProperties(ValueTree sfzRegion, ValueT
         }
     }
 }
+
+void DSPresetConverter::parseDSEXS24(DSEXS24 exs24, juce::String samplePath, juce::File outputDir) {
+    juce::Array<DSEXS24Zone> &zones = exs24.getZones();
+    juce::Array<DSEXS24Group> &groups = exs24.getGroups();
+    juce::Array<DSEXS24Sample> &samples = exs24.getSamples();
+    valueTree = juce::ValueTree("DecentSampler");
+    valueTree.setProperty("pluginVersion", 1, nullptr);
+    juce::ValueTree groupsVT = juce::ValueTree("groups");
+    valueTree.appendChild(groupsVT, nullptr);
+    
+    for (DSEXS24Zone zone : zones) {
+        if(zone.groupIndex < 0) {
+            continue;
+        } else if(zone.groupIndex > 100) {
+            printf("This zone's group index is greater than 100. This converter may not support this file.");
+            continue;
+        }
+        while (zone.groupIndex >= groups.size()) {
+            DSEXS24Group newGroup;
+            newGroup.name = "Couldn't find group index " + juce::String(zone.groupIndex);
+            groups.add(newGroup);
+        }
+    }
+    
+    // Iterate through the groups and add their respective samples
+    for(int groupIndex = -1; groupIndex < groups.size(); groupIndex++) {
+        bool hasSamples = false;
+        DSEXS24Group group = (groupIndex >= 0) ? groups[groupIndex] : DSEXS24Group();
+        juce::ValueTree dsGroup ("group");
+        if(group.name != "") {
+            dsGroup.setProperty("name", group.name, nullptr);
+        }
+//        dsGroup.setProperty("_exsGroupIndex", groupIndex, nullptr);
+        if(group.pan != 0) {
+            dsGroup.setProperty("pan", group.pan, nullptr);
+        }
+        if(group.volume != 0) {
+            dsGroup.setProperty("volume", juce::String(group.volume) + "dB", nullptr);
+        }
+        
+        if(group.seqNumber != 0) {
+            dsGroup.setProperty("seqPosition", group.seqNumber, nullptr);
+        }
+//        dsGroup.setProperty("exsSequence", group.exsSequence, nullptr);
+        
+        for (DSEXS24Zone zone : zones) {
+            if (zone.groupIndex == groupIndex) {
+                juce::ValueTree dsSample("sample");
+                
+                int sampleIndex = zone.sampleIndex;
+                if(sampleIndex < 0 || sampleIndex >= samples.size()) {
+                    continue;
+                }
+                
+                DSEXS24Sample &sample = samples.getReference(sampleIndex);
+                if(samplePath.isNotEmpty()) {
+                    juce::File sampleFile = juce::File::getCurrentWorkingDirectory().getChildFile (samplePath).getChildFile(sample.fileName);
+                    dsSample.setProperty("path", sampleFile.getRelativePathFrom(outputDir), nullptr);
+                } else {
+                    juce::File sampleFile = juce::File(sample.filePath).getChildFile(sample.fileName);
+                    dsSample.setProperty("path", sampleFile.getRelativePathFrom(outputDir), nullptr);
+                }
+                    
+                dsSample.setProperty("name", zone.name, nullptr);
+                if(zone.pitch == false) {
+                    dsSample.setProperty("pitchKeyTrack", 0, nullptr);
+                }
+                dsSample.setProperty("rootNote", zone.key, nullptr);
+                dsSample.setProperty("loNote", zone.keyLow, nullptr);
+                dsSample.setProperty("hiNote", zone.keyHigh, nullptr);
+                dsSample.setProperty("loVel", zone.velocityRangeOn ? zone.loVel : 0, nullptr);
+                dsSample.setProperty("hiVel", zone.velocityRangeOn ? zone.hiVel : 127, nullptr);
+                
+                float tuning = (float) zone.coarseTuning + (((float)zone.fineTuning)/100.0f);
+                if(tuning != 0) {
+                    dsSample.setProperty("tuning", tuning, nullptr);
+                }
+                if(zone.pan != 0) {
+                    dsSample.setProperty("pan", zone.pan, nullptr);
+                }
+                if(zone.volume != 0) {
+                    dsSample.setProperty("volume", juce::String(zone.volume) + "dB", nullptr);
+                }
+                
+                if(zone.sampleStart != 0) {
+                    dsSample.setProperty("start", zone.sampleStart, nullptr);
+                }
+                
+                if(zone.sampleEnd != 0) {
+                    dsSample.setProperty("end", zone.sampleEnd, nullptr);
+                }
+                
+                if(zone.loopEnabled) {
+                    dsSample.setProperty("loopEnabled", zone.loopEnabled, nullptr);
+                    dsSample.setProperty("loopStart", zone.loopStart, nullptr);
+                    dsSample.setProperty("loopEnd", zone.loopEnd, nullptr);
+                
+                    if(zone.loopCrossfade != 0) {
+                        dsSample.setProperty("loopCrossfade", zone.loopCrossfade, nullptr);
+                    }
+                    
+                    dsSample.setProperty("loopCrossfadeMode", zone.loopEqualPower ? "equal_power" : "linear", nullptr);
+                }
+                
+                
+                hasSamples = true;
+                dsGroup.appendChild(dsSample, nullptr);
+            }
+        }
+        if(hasSamples) {
+            groupsVT.appendChild(dsGroup, nullptr);
+        }
+    }
+    
+}
+
--- Source/DSPresetConverter.h
+++ Source/DSPresetConverter.h
@@ -1,8 +1,8 @@
 /*
   ==============================================================================
 
-    DSPresetMaker.h
     Created: 18 Oct 2020 10:30:32pm
+    DSPresetConverter.h
     Author:  David Hilowitz
 
   ==============================================================================
@@ -10,14 +10,21 @@
 
 #pragma once
 
-#include <JuceHeader.h>
+#include "DSEXS24.h"
 
 class DSPresetConverter {
 public:
-    void parseSFZValueTree(ValueTree valueTree);
-    String getXML() {
-        return valueTree.toXmlString();
+    void parseDSEXS24(DSEXS24 exs24, juce::String samplePath, juce::File outputDir);
+    void parseSFZValueTree(juce::ValueTree valueTree);
+    juce::String getXML() {
+        juce::XmlElement::TextFormat format;
+        format.lineWrapLength = 20000;
+//        format.newLineChars = "";
+        return valueTree.toXmlString(format);
     }
+    
+    juce::ValueTree getValueTree() { return valueTree; }
+    std::unique_ptr<juce::XmlElement> getXMLObject() { return valueTree.createXml(); }
     
     enum HeaderLevel {
         headerLevelGlobal,
@@ -25,6 +32,6 @@ public:
         headerLevelRegion
     };
 private:
-    ValueTree valueTree;
-    void translateSFZRegionProperties(ValueTree sfzRegion, ValueTree &dsSample, HeaderLevel level);
+    juce::ValueTree valueTree;
+    void translateSFZRegionProperties(juce::ValueTree sfzRegion, juce::ValueTree &dsSample, HeaderLevel level);
 };
